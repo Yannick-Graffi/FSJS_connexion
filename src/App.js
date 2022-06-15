18a6770458f391bc9507d5b839ddc57fe0eab176
Yannick <yann001@hotmail.com>
2022-06-15 10:10:38 +0200

    link in the component connexion deplaced to app.js to use the link to show/hide the register component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './components/register/register';
+import Connexion from './components/connexion/connexion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <section>
+        <Connexion>
+          <a href="#">Pas de compte ?</a>
+        </Connexion>
 
-      
+        <Register />
+      </section>
     </div>
   );
 }
--- src/components/connexion/connexion.js
+++ src/components/connexion/connexion.js
@@ -1,12 +1,12 @@
 
 
-function Connexion() {
+function Connexion(props) {
     return ( 
         <div className="connexion-container">
             <h2>Connexion</h2>
             <input type="text" className="connexionId">Identifiant</input>
             <input type="text" className="connexionPassword">Mot de passe</input>
-            <a href="#">Pas de compte ?</a>
+            {props.children}
             <button className="loginBtn">Connexion</button>    
         </div>
 
